ea03e2cf36bedc12b24e4ffff735eec2d49ed968
Artyom <45438330+EnergyWork@users.noreply.github.com>
2020-03-22 01:47:04 +0300

    upd

--- lab5/task3/task3.js
+++ lab5/task3/task3.js
@@ -1,14 +1,15 @@
 let outputField = document.querySelector("#outputfield");
 let backets = 0;
 let operators = ["+","-","*","/",".","!","**","^","√"];
-let evalResult = 0;
+let evalResult = "0";
+let floatNum = false;
 
 function factorial(n) {
     return (n != 1) ? n * factorial(n - 1) : 1;
 }
 
 function reset() {
-    outputField.value = evalResult = "0"; 
+    outputField.value = "0"; 
 }
 
 function eraselast() {
@@ -18,7 +19,7 @@ function eraselast() {
         broket--;
     evalResult = outputField.value = outputField.value.slice(0, outputField.value.length - 1);
     if (outputField.value.length == 0) {
-        outputField.value = evalResult = "0";
+        outputField.value = "0";
     }
 }
 
@@ -27,13 +28,55 @@ function createStrToEval(str) {
 }
 
 function addValue(val) {
-    if (parseInt(val) || val == "0") {
+    if (parseInt(val, 10)) { // digit
         if(outputField.value.slice(-1) == "0" && outputField.value.length == 1) {
             outputField.value = val;
-            evalResult = val;
-        } else {
+        } else if (!")!".includes(outputField.value.slice(-1))) {
             outputField.value += val;
-            evalResult += val;
+        }
+    }
+    else { //! ( ) - ^ + sqrt . /
+        switch(val) {
+            case '/': 
+            case '*': 
+            case '-': 
+            case '+': 
+                if (!"(√.^+-*/".includes(outputField.value.slice(-1))) {
+                    outputField.value += val;
+                }
+                break;
+            case '√': 
+                if (!"0123456789!^)".includes(outputField.value.slice(-1))) {
+                    outputField.value += val;
+                } else if (outputField.value.length == 1 && outputField.value.slice(-1) == "0") {
+                    outputField.value = val;
+                }
+                break;
+            case '(': 
+                if (!"0123456789.!)".includes(outputField.value.slice(-1))) {
+                    outputField.value += val;
+                }
+                break;
+            case ')': 
+                if (!".+-*/√^(".includes(outputField.value.slice(-1))) {
+                    outputField.value += val;
+                }
+                break; 
+            case '!': 
+                if (!".+-*/√(^".includes(outputField.value.slice(-1))) {
+                    outputField.value += val;
+                }
+                break;
+            case '^': 
+                if (!".+-*/√".includes(outputField.value.slice(-1))) {
+                    outputField.value += val;
+                }
+                break;
+            case '.': 
+                if ("0123456789.".includes(outputField.value.slice(-1)) && !outputField.value.includes('.')) {
+                    outputField.value += val;
+                }
+                break;
         }
     }
 }
